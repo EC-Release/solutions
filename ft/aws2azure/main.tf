2c1221a9f9f2945c2e204a92d733b6a403ec3323
ramaraosrikakulapu <Ramarao.Srikakulapu@ge.com>
2020-09-15 15:45:44 -0400

    scripts update for instance creation in aws and azure

--- ft/aws2azure/main.tf
+++ ft/aws2azure/main.tf
@@ -8,132 +8,14 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "RG-ram"
-  location = "East US"
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "vnet-ram"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_network_security_group" "example" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  security_rule {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  tags = {
-    environment = "Production"
-  }
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "subnet-ram"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes      = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "example" {
-  name                    = "publicip-ram"
-  location                = azurerm_resource_group.example.location
-  resource_group_name     = azurerm_resource_group.example.name
-  allocation_method       = "Dynamic"
-  idle_timeout_in_minutes = 30
-
-  tags = {
-    environment = "test"
-  }
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "nic-ram"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "ramconfiguration1"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.2.5"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
-
-resource "azurerm_virtual_machine" "example" {
-  name                  = "ram-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_D2s_v3"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "asrikar"
-    admin_password = "Sairam81!@#$"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-//  os_profile_secrets {
-
-//  }
-  tags = {
-    "Name" = "EC-File-Transfer"
-	"UAI" = "UAI3023216"
-	"Engineer.SSO" = "212602085"
-	"Engineer.Email" = "RamaRao.Srikakulapu@ge.com"
-	"Env" = "Dev"
-  }
-}
-
-//data "azurerm_public_ip" "example" {
-//  name                = azurerm_public_ip.example.name
-//  resource_group_name = azurerm_virtual_machine.example.resource_group_name
-//}
-
-resource "aws_instance" "dc-sandbox-vpc-vm1" {
-  ami                         = var.dc-sandbox-vpc-vm.ami
-  instance_type               = var.dc-sandbox-vpc-vm.instance_type
-  subnet_id                   = var.dc-sandbox-vpc-vm.subnet_id
-  security_groups             = var.dc-sandbox-vpc-vm.security_groups
-  key_name                    = var.dc-sandbox-vpc-vm.key_name
-  iam_instance_profile        = var.dc-sandbox-vpc-vm.iam_instance_profile
-  associate_public_ip_address = var.dc-sandbox-vpc-vm.associate_public_ip_address
+resource "aws_instance" "aws-dc-sandbox-vpc-vm1" {
+  ami                         = var.aws-dc-sandbox-vpc-vm.ami
+  instance_type               = var.aws-dc-sandbox-vpc-vm.instance_type
+  subnet_id                   = var.aws-dc-sandbox-vpc-vm.subnet_id
+  security_groups             = var.aws-dc-sandbox-vpc-vm.security_groups
+  key_name                    = var.aws-dc-sandbox-vpc-vm.key_name
+  iam_instance_profile        = var.aws-dc-sandbox-vpc-vm.iam_instance_profile
+  associate_public_ip_address = var.aws-dc-sandbox-vpc-vm.associate_public_ip_address
   user_data                   = <<-EOF
             #! /bin/bash
             sudo su -
@@ -143,7 +25,7 @@ resource "aws_instance" "dc-sandbox-vpc-vm1" {
             chkconfig httpd on >> ~/logs.txt
             cd /var/www/html >> ~/logs.txt
             echo "<html><h1>test webpage</h1></html>" > index.html
-            ${var.dc-sandbox-vpc-vm.scp_command}
+            ${var.aws-dc-sandbox-vpc-vm.scp_command}
    EOF
 
   root_block_device {
@@ -151,12 +33,89 @@ resource "aws_instance" "dc-sandbox-vpc-vm1" {
   }
 
   tags = {
-    Name           = var.dc-sandbox-vpc-vm.tags.Name
-    Env            = var.dc-sandbox-vpc-vm.tags.Env
-    Engineer_Email = var.dc-sandbox-vpc-vm.tags.Engineer_Email
-    Engineer_SSO   = var.dc-sandbox-vpc-vm.tags.Engineer_SSO
-    Best_By        = var.dc-sandbox-vpc-vm.tags.Best_By
-    UAI            = var.dc-sandbox-vpc-vm.tags.UAI
-    Builder        = var.dc-sandbox-vpc-vm.tags.Builder
+    Name           = var.aws-dc-sandbox-vpc-vm.tags.Name
+    Env            = var.aws-dc-sandbox-vpc-vm.tags.Env
+    Engineer_Email = var.aws-dc-sandbox-vpc-vm.tags.Engineer_Email
+    Engineer_SSO   = var.aws-dc-sandbox-vpc-vm.tags.Engineer_SSO
+    Best_By        = var.aws-dc-sandbox-vpc-vm.tags.Best_By
+    UAI            = var.aws-dc-sandbox-vpc-vm.tags.UAI
+    Builder        = var.aws-dc-sandbox-vpc-vm.tags.Builder
   }
 }
+
+resource "azurerm_network_interface" "azure-corp-ec-ft-nic" {
+  name                = var.azure-corp-ec-ft-nic.name
+  location            = var.azure-corp-ec-ft-nic.location
+  resource_group_name = var.azure-corp-ec-ft-nic.resource_group_name
+
+  ip_configuration {
+    name                          = var.azure-corp-ec-ft-nic.ip_configuration.name
+    subnet_id                     = var.azure-corp-ec-ft-nic.ip_configuration.subnet_id
+    private_ip_address_allocation = var.azure-corp-ec-ft-nic.ip_configuration.private_ip_address_allocation
+    # Enable this when public ip allowed in corp subscription
+    # public_ip_address_id          = azurerm_public_ip.ec-vm-publicip.id
+  }
+}
+
+resource "azurerm_virtual_machine" "azure-corp-ec-vm" {
+  name                  = var.azure-corp-ec-vm.name
+  location              = var.azure-corp-ec-vm.location
+  resource_group_name   = var.azure-corp-ec-vm.resource_group_name
+  network_interface_ids = [azurerm_network_interface.azure-corp-ec-ft-nic.id]
+  vm_size               = var.azure-corp-ec-vm.vm_size
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination     = var.azure-corp-ec-vm.delete_os_disk_on_termination
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination  = var.azure-corp-ec-vm.delete_data_disks_on_termination
+
+  storage_image_reference {
+    publisher         = var.azure-corp-ec-vm.storage_image_reference.publisher
+    offer             = var.azure-corp-ec-vm.storage_image_reference.offer
+    sku               = var.azure-corp-ec-vm.storage_image_reference.sku
+    version           = var.azure-corp-ec-vm.storage_image_reference.version
+  }
+  storage_os_disk {
+    name              = var.azure-corp-ec-vm.storage_os_disk.name
+    caching           = var.azure-corp-ec-vm.storage_os_disk.caching
+    create_option     = var.azure-corp-ec-vm.storage_os_disk.create_option
+    managed_disk_type = var.azure-corp-ec-vm.storage_os_disk.managed_disk_type
+  }
+  os_profile {
+    computer_name     = var.azure-corp-ec-vm.os_profile.computer_name
+    admin_username    = var.azure-corp-ec-vm.os_profile.admin_username
+    admin_password    = var.azure-corp-ec-vm.os_profile.admin_password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = {
+    Name           = var.azure-corp-ec-vm.tags.Name
+    Env            = var.azure-corp-ec-vm.tags.Env
+    Engineer_Email = var.azure-corp-ec-vm.tags.Engineer_Email
+    Engineer_SSO   = var.azure-corp-ec-vm.tags.Engineer_SSO
+    Best_By        = var.azure-corp-ec-vm.tags.Best_By
+    UAI            = var.azure-corp-ec-vm.tags.UAI
+    Builder        = var.azure-corp-ec-vm.tags.Builder
+  }
+}
+
+// Uncomment below section when corp subscription allowed public ip
+//resource "azurerm_public_ip" "ec-vm-publicip" {
+//  name                    = "publicip-ram"
+//  location                = "eastus"
+//  resource_group_name     = "cs-connectedVNET"
+//  allocation_method       = "Dynamic"
+//  idle_timeout_in_minutes = 30
+//
+//  tags = {
+//    environment = "test"
+//  }
+//}
+//
+//data "azurerm_public_ip" "example" {
+//  name                = azurerm_public_ip.ec-vm-publicip.name
+//  resource_group_name = azurerm_virtual_machine.main.resource_group_name
+//}
--- ft/aws2azure/variables.tf
+++ ft/aws2azure/variables.tf
@@ -6,7 +6,7 @@ variable "aws_region" {
   type = string
 }
 
-variable "dc-sandbox-vpc-vm" {
+variable "aws-dc-sandbox-vpc-vm" {
   type = object({
     ami                         = string
     instance_type               = string
@@ -15,7 +15,34 @@ variable "dc-sandbox-vpc-vm" {
     key_name                    = string
     iam_instance_profile        = string
     associate_public_ip_address = bool
-    scp_command                = string
+    scp_command                 = string
     tags                        = map(string)
+  })
+}
+
+variable "azure-corp-ec-ft-nic" {
+  type = object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    ip_configuration    = map(string)
+  })
+}
+
+variable "azure-corp-ec-vm" {
+  type = object({
+    name                              = string
+    location                          = string
+    resource_group_name               = string
+    network_interface_ids             = list(string)
+    vm_size                           = string
+    delete_os_disk_on_termination     = bool
+    delete_data_disks_on_termination  = bool
+
+    storage_image_reference           = map(string)
+    storage_os_disk                   = map(string)
+    os_profile                        = map(string)
+    os_profile_linux_config           = map(string)
+    tags                              = map(string)
   })
 }
